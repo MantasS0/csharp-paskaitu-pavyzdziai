e295ea63294359c91bb76bdf2cd06807dbbde064
Ieva <ieva.jaksaityte@gmail.com>
2018-10-09 10:32:41 +0300

    isvedimas

---
+++ 2-21 pavyzdys/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_21_pavyzdys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gyvunas = "suo";
+            Console.WriteLine("gyvunas: {0}", gyvunas);
+            gyvunas = "kate";
+            Console.WriteLine("pakeistas gyvunas: {0}", gyvunas);
+        }
+    }
+}
---
+++ 2-22 pavyzdys/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_22_pavyzdys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius;
+            Console.WriteLine("skaicius dar neivestas");
+            skaicius = 4;
+            Console.WriteLine("skaicius = {0}", skaicius);
+            skaicius = 9;
+            Console.WriteLine("skaicius pakeistas i {0}", skaicius);
+        }
+    }
+}
