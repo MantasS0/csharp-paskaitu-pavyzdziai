9772c88662e2730bafea7a968aa5d1ce4eaad599
Ieva <ieva.jaksaityte@gmail.com>
2018-10-17 12:03:18 +0300

    vienmatis masyvas

--- 11-12 pavyzdys/Program.cs
+++ 11-12 pavyzdys/Program.cs
@@ -17,15 +17,12 @@ namespace _11_12_pavyzdys
             Console.WriteLine("skaiciu masyvo dydis: " + skaiciai.Length);
             Console.WriteLine("simboliu masyvo dydis: " + simboliai.Length);
             Console.WriteLine();
-
             Console.WriteLine("pirmas skaicius: " + skaiciai.First());
             Console.WriteLine("paskutinis skaicius: " + skaiciai.Last());
             Console.WriteLine();
-
             Console.WriteLine("pirmas ir paskutinis simboliai: {0} {1}",
                 simboliai.First(), simboliai.Last());
             Console.WriteLine();
-
             Console.WriteLine("paskutinis skaicius: " + skaiciai[skaiciai.Length - 1]);
             Console.WriteLine("paskutinis simbolis: " + simboliai[simboliai.Length - 1]);
         }
