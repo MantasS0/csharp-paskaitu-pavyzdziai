debc50b1172a006260ca41068587f4b7a8fd1426
Ieva <ieva.jaksaityte@gmail.com>
2018-10-18 10:34:39 +0300

    pavyddys

--- 12-1 pavyzdys/Program.cs
+++ 12-1 pavyzdys/Program.cs
@@ -10,6 +10,18 @@ namespace _12_1_pavyzdys
     {
         static void Main(string[] args)
         {
+            int [,] skaiciai = new int [3,4] {
+                {0, 1, 2, 3} ,   /*  eile nr 0 */
+                {4, 5, 6, 7} ,   /*  eile nr 1 */
+                {8, 9, 10, 11}   /*  eile nr 2 */
+            };
+
+            for (var i = 0; i < 3; i++) {
+            
+                for (var j = 0; j < 4; j++) {
+                    Console.WriteLine("a[{0},{1}] = {2}", i, j, skaiciai[i,j]);
+                }
+            }
         }
     }
 }
