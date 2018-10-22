a530c005dc80fdf98948d3c4e27df8a30d97f506
Ieva <ieva.jaksaityte@gmail.com>
2018-10-22 03:00:31 +0300

    klasiu pavyzdziai

---
+++ 20-9 pavyzdys/Automobiliai.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_9_pavyzdys
+{
+    class Automobiliai
+    {
+        public List<Automobilis> AutomobiliuSarasas { get; set; }
+
+        public Automobiliai(List<Automobilis> automobiliuSarasas)
+        {
+            AutomobiliuSarasas = automobiliuSarasas;
+        }
+
+        public Automobiliai(string failas)
+        {
+            var tekstas = File.ReadAllLines(failas);
+            AutomobiliuSarasas = new List<Automobilis>();
+
+            for (int i = 1; i < tekstas.Length; i++)
+            {
+                var auto = new Automobilis(tekstas[i]);
+                AutomobiliuSarasas.Add(auto);
+            }
+        }
+
+        public void Isvedimas()
+        {
+            Console.WriteLine("{0, -12} {1, -12} {2, -12} {3, -12} {4, -12} {5, -12}",
+                "Marke", "Modelis", "Rida, km", "Metai", "D. turis l.", "Kuro tipas");
+
+            foreach (var auto in AutomobiliuSarasas)
+            {
+                auto.Spausdinti();
+            }
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Naujausias automobilis");
+            NaujausiasAutomobilis().Spausdinti();
+
+            Console.WriteLine("Didziausia rida");
+            var didziausiaRida = DidziausiaRida();
+            didziausiaRida.Spausdinti();
+
+            Console.WriteLine("Maziausias darbinis turis");
+            MaziausiasDarbinisTuris().Spausdinti();
+
+            Console.WriteLine();
+        }
+
+        public Automobilis NaujausiasAutomobilis()
+        {
+            var naujausias = AutomobiliuSarasas.First();
+
+            foreach (var auto in AutomobiliuSarasas)
+            {
+                if (!naujausias.SitasNaujesnis(auto))
+                {
+                    naujausias = auto;
+                }
+            }
+
+            return naujausias;
+        }
+
+        public Automobilis DidziausiaRida()
+        {
+            var didziausia = AutomobiliuSarasas.First();
+
+            foreach (var auto in AutomobiliuSarasas)
+            {
+                if (auto.Rida > didziausia.Rida)
+                {
+                    didziausia = auto;
+                }
+            }
+
+            return didziausia;
+        }
+
+        public Automobilis MaziausiasDarbinisTuris()
+        {
+            var maziausias = AutomobiliuSarasas.First();
+
+            foreach (var auto in AutomobiliuSarasas)
+            {
+                if (auto.DarbinisTuris < maziausias.DarbinisTuris)
+                {
+                    maziausias = auto;
+                }
+            }
+
+            return maziausias;
+        }
+    }
+}
---
+++ 20-9 pavyzdys/Automobilis.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_9_pavyzdys
+{
+    class Automobilis
+    {
+        public string Marke { get; set; }
+        public string Modelis { get; set; }
+        public int Rida { get; set; }
+        public int Metai { get; set; }
+        public double DarbinisTuris { get; set; }
+        public string KuroTipas { get; set; }
+
+        public Automobilis(string marke, string modelis, int rida, int metai, double darbinisTuris, string kuroTipas)
+        {
+            Marke = marke;
+            Modelis = modelis;
+            Rida = rida;
+            Metai = metai;
+            DarbinisTuris = darbinisTuris;
+            KuroTipas = kuroTipas;
+        }
+
+        public Automobilis(string eilute)
+        {
+            var duomenys = eilute.Split(';');
+            Marke = duomenys[0];
+            Modelis = duomenys[1];
+            Rida = Convert.ToInt32(duomenys[2]);
+            Metai = Convert.ToInt32(duomenys[3]);
+            DarbinisTuris = Convert.ToDouble(duomenys[4]);
+            KuroTipas = duomenys[5];
+        }
+
+        public void Spausdinti()
+        {
+            Console.WriteLine("{0, -12} {1, -12} {2, -12} {3, -12} {4, -12} {5, -12}",
+                Marke, Modelis, Rida, Metai, DarbinisTuris, KuroTipas);
+        }
+
+        public bool SitasNaujesnis(Automobilis kitas)
+        {
+            if (Metai > kitas.Metai)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
